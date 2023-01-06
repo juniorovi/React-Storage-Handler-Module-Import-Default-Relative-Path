3917864500d2bf9caf6554fd3bf6ccc22614c338
juniorovi <mdtarikulislam258000@gmail.com>
2023-01-07 00:30:53 +0600

    Duplicate check in local starage To Store Multiple Data

--- src/Utilities/fakedb.js
+++ src/Utilities/fakedb.js
@@ -1,9 +1,13 @@
 // Use local storage to manage cart data
 const addToDb = id => {
-    if (localStorage.getItem(id)) {
+    const quantity = localStorage.getItem(id)
+    if (quantity) {
         console.log('already Exists');
+        const newQuantity = parseInt(quantity) + 1;
+        localStorage.setItem(id, newQuantity);
     }
     else {
+        console.log('New items');
         localStorage.setItem(id, 1);
     }
 
