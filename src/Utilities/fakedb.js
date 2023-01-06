bff5f9a1a4d97f8b0ddb744d389eb60f61cf9a49
juniorovi <mdtarikulislam258000@gmail.com>
2023-01-06 23:57:38 +0600

    To Store Data Just One Item into Local Storage by a central utility folder fakeDb.js file

---
+++ src/Utilities/fakedb.js
@@ -0,0 +1,5 @@
+// Use local storage to manage cart data
+const addToDb = id =>{
+    localStorage.setItem(id, 1);
+}
+export {addToDb}
--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { addToDb } from '../../Utilities/fakedb';
 import './Cosmetic.css'
 const Cosmetic = (porps) => {
     const { name, price, id } = porps.cosmetic;
-    const addToCart = (id) => {
-        localStorage.setItem(id, 1)
-    }
+
+    /*     const addToCart = (id) => {
+            localStorage.setItem(id, 1)
+        } */
+
     // const addToCartWithParameter =()=> addToCart(id);
+
     return (
         <div className='product'>
             {/* <h2>Buy this: {porps.cosmetic.name}</h2> */}
@@ -13,7 +17,7 @@ const Cosmetic = (porps) => {
             <p>Only for: ${price}</p>
             <p><small>It has an ID: {id}</small></p>
             {/* <button onClick={addToCartWithParameter}>Add to Cart</button> */}
-            <button onClick={() => addToCart(id)}>Add to cart Shortcut</button>
+            <button onClick={() => addToDb(id)}>Add to cart Shortcut</button>
         </div>
     );
 };
