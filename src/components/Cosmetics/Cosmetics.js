9e46c569c89c5a03b3c6275e587d98a68b9cafde
juniorovi <mdtarikulislam258000@gmail.com>
2023-01-06 21:43:53 +0600

    3rd way data loded from my own data.json file in public folde

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,34 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cosmetic from '../Cosmetic/Cosmetic';
 
 const Cosmetics = () => {
-    const cosmetics = [
-        {
-            "id": "63b82e7bd2a821e15f36e8cf",
-            "price": "$2,128.91",
-            "name": "Warner Pope"
-        },
-        {
-            "id": "63b82e7b187f031baa46d465",
-            "price": "$1,004.25",
-            "name": "Lilly Pratt"
-        },
-        {
-            "id": "63b82e7bbe65780e29c895f4",
-            "price": "$1,072.22",
-            "name": "Cameron Rose"
-        },
-        {
-            "id": "63b82e7b86ae1cf6b255a61e",
-            "price": "$2,586.85",
-            "name": "Dale Snyder"
-        },
-        {
-            "id": "63b82e7b050bf898d41c34e1",
-            "price": "$2,665.63",
-            "name": "Gomez Short"
-        }
-    ]
+    const [cosmetics, setCosmetics]=useState([]);
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res=>res.json())
+        .then(data =>setCosmetics(data))
+    },[])
 
     return (
         <div>
