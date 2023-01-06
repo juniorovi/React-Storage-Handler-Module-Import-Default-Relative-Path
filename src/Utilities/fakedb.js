edda9d01d20970c3d1be89ef60260407dc63555f
juniorovi <mdtarikulislam258000@gmail.com>
2023-01-07 00:16:01 +0600

    Duplicate check in local starage To Store Data Just One Item into Local Storage by a central utility folder fakeDb.js file

--- src/Utilities/fakedb.js
+++ src/Utilities/fakedb.js
@@ -1,5 +1,11 @@
 // Use local storage to manage cart data
-const addToDb = id =>{
-    localStorage.setItem(id, 1);
+const addToDb = id => {
+    if (localStorage.getItem(id)) {
+        console.log('already Exists');
+    }
+    else {
+        localStorage.setItem(id, 1);
+    }
+
 }
-export {addToDb}
+export { addToDb }
